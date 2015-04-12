087f4a98bf5f18c81d6fa4c30dbdb788bac7bcd9
Gruuler <nielson.cj@gmail.com>
2015-04-11 18:03:20 -0600

    logout.php
    Filled in logout.php

--- logout.php
+++ logout.php
@@ -1,3 +1,10 @@
 <?php
 
+	setcookie('userId','', time() - 3600);
+	setcookie('user','', time() - 3600);
+	setcookie('time','', time() - 3600);
+	setcookie('hash','', time() - 3600);
+
+	header ("location:main.php");
+
 ?>
