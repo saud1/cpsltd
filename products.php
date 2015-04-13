8d8cef5f278f85331f38552b373e506ea08d9e20
Chris Nielson <nielson.cj@gmail.com>
2015-04-13 14:11:58 -0600

    products formatting
    Formatted the products.php to look nicer.

--- products.php
+++ products.php
@@ -1,14 +1,36 @@
 <!doctype html>
 <html>
 <?php
-$title = 'Products';
-include 'header.php';
 
 // Main product page.  This will have lists of the products that are currently offered in the
 // store, with options to narrow down product selection.
 
-echo '<p>We have no products to display currently.</p>';
+$title = 'Products';
+include 'header.php';
+?>
 
+<div class="title"><h2>Products<h2></div>
+
+<div class = "product-center">
+	<div class = "inline">
+		<div class = "col-md-3">
+			<div class="search">	
+				<form>
+					Search for Products:
+					<input type="text" name="search">
+					<input type="button" value="search">
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="inline">
+		<div class = "col-md-9">
+			We have no products to display.
+		</div>
+	</div>
+</div>
+
+<?php
 include 'footer.php';
 ?>
 </htm>
