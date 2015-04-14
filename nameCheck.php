814574de66aa8511468b38382b22e58dd74aac15
Chris Nielson <nielson.cj@gmail.com>
2015-04-14 16:25:30 -0600

    name check file
    File to check name, javascript

---
+++ nameCheck.php
@@ -0,0 +1,20 @@
+<?php
+
+// Checks the database for a user's name.
+
+include 'connect.php';
+
+if(empty($_GET['user'])) {
+	die("Invalid user name submitted.")
+}
+
+$user = $_GET["user"];
+$sql = "SELECT UserId FROM users WHERE Username = '$user'"; 
+$results = mysqli_query($link,$sql);
+if(mysqli_num_rows($results)>0) {
+	echo 1;
+}else{
+	echo 0;
+}
+
+?>
