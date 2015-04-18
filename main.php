af4b680c9e9a0eac15e97f79531f7d05bf16b93e
Pamela21 <Pamela21@users.noreply.github.com>
2015-04-18 13:25:56 -0600

    Update main.php

--- main.php
+++ main.php
@@ -9,6 +9,7 @@ include 'header.php';
 
 echo '<div class="title"><h1>Welcome to CPS LTD</h1></div>';
 echo '<p>Looking for something in particular?</p>';
+<button type="button" onclick="myFunction()">Try it</button>
 
 include 'footer.php';
 ?>
