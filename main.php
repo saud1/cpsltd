d430443dcb8460b9e060fbcab5f4e23ad346c451
Pamela21 <Pamela21@users.noreply.github.com>
2015-04-18 13:28:43 -0600

    Update main.php

--- main.php
+++ main.php
@@ -9,8 +9,22 @@ include 'header.php';
 
 echo '<div class="title"><h1>Welcome to CPS LTD</h1></div>';
 echo '<p>Looking for something in particular?</p>';
-<button type="button" onclick="myFunction()">Try it</button>
+<button type="button" onclick="myFunction()">Try it</button>;
+
+<div class='title'><h2>Sign up for a user account today!</h2></div><br><br>
+<?php
+if(isset($_GET['reg'])) {
+$check = false;
+}else{
+$check = true;
+}
+if($check){
+echo "<br><h4>Account created successfully. <a href='login.html'>Click here to log in</a>.</h4><br>";
+}
+?>
 
 include 'footer.php';
 ?>
 </html>
+
+
