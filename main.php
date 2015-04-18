d53f184bd7e489adc1adcd14e3b1d7e4d463929c
Pamela21 <Pamela21@users.noreply.github.com>
2015-04-18 13:58:18 -0600

    Update main.php

--- main.php
+++ main.php
@@ -8,12 +8,11 @@ $title = 'Main';
 include 'header.php';
 
 echo '<div class="title"><h1>Welcome to CPS LTD</h1></div>';
-echo '<p>Looking for something in particular?</p>';
-<button type="button" onclick="myFunction()">Try it</button>;
-
+echo '<p>What are you looking for?</p>';
 
 <script type='text/javascript'>var user=userName</script>
 <script type='text/javascript'>
+var register = site.Registry;
 <div class='title'><h2>Sign up for a user account today!</h2></div><br><br>
 <?php
 if(isset($_GET['reg'])) {
@@ -25,6 +24,13 @@ if($check){
 echo "<br><h4>Account created successfully. <a href='login.html'>Click here to log in</a>.</h4><br>";
 }
 ?>
+
+</form>
+	<h1 class="items">Your CPS Shopping Cart</h1>
+<div class="fl">
+	<h2 class="items ">Your CPS Shopping Cart</h2>
+</div>
+
 
 
 include 'footer.php';
