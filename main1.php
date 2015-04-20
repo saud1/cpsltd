722fc68ab7a678f091d08dd69b09a2c6ac3f60b4
Chris Nielson <nielson.cj@gmail.com>
2015-04-20 16:50:12 -0600

    fixed main1.php
    fixed main1.php

---
+++ main1.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<?php
+$title="main";
+include 'header.php';
+?>
+
+</head>
+<body>
+<form method="post" action="process.php">
+</form>
+ 
+ <div class=title><h1>CPS LTD welcomes you</h1></div>
+<p> what are you looking for?</p>
+ 
