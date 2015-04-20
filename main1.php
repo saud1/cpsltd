7ec4438a4f6b53ed1b84d6b596c46a9fbae69b32
Pamela21 <Pamela21@users.noreply.github.com>
2015-04-20 17:20:56 -0600

    Update main1.php

--- main1.php
+++ main1.php
@@ -7,10 +7,33 @@ include 'header.php';
 
 </head>
 <body>
-<form method="post" action="process.php">
+<form method="post" action="header.php">
 </form>
  
  <div class=title><h1>CPS LTD welcomes you</h1></div>
 <p> what are you looking for?</p>
 
 include "login.php"
+
+<form method="post" action="login.php">
+Username <input type="text" name="userName"><br>
+Password <input type="password" name="pwd"><br>
+<input type="submit" value="Go">
+</form>
+
+include "registeration.php"
+<script type='text/javascript'>var user=userName</script>
+<script type='text/javascript'>
+var register = account.Registration;
+<div class='title'><h2>Sign up for a user account today!</h2></div><br><br>
+<?php
+if(isset($_GET['reg'])) {
+$check = false;
+}else{
+$check = true;
+}
+if($check){
+echo "<br><h4>Account created successfully. <a href='login.html'>Click here to log in</a>.</h4><br>";
+}
+?>
+
