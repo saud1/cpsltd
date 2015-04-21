4b908e3075486389968bed2c28bb601ddc17ede2
Pamela21 <Pamela21@users.noreply.github.com>
2015-04-20 18:11:05 -0600

    Update main1.php

--- main1.php
+++ main1.php
@@ -7,8 +7,7 @@ include 'header.php';
 
 </head>
 <body>
-<form method="post" action="header.php">
-</form>
+
  
  <div class=title><h1>CPS LTD welcomes you</h1></div>
 <p> what are you looking for?</p>
