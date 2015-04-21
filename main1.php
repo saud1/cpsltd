75ae8bbb6ddbf696a79592babcb4ab2f073be892
Pamela21 <Pamela21@users.noreply.github.com>
2015-04-20 18:24:35 -0600

    Update main1.php

--- main1.php
+++ main1.php
@@ -10,9 +10,9 @@ include 'header.php';
 
 <?php
 include "products.php";
+?>
 
-<form method="post" action="products.php>"
-</form>
+<form>
 search <input type='text' name='pCat'>
 <select name='pCat'>
   <option value='0'>All</option>
@@ -21,15 +21,11 @@ search <input type='text' name='pCat'>
 </select>
 <input type='submit' value='Go'>
 </form>
-<?php
-include("sessions/connect.php");
-$sTerm="";
-if(!empty($_GET["pCat"])){
-  $sTerm = $_GET["pCat"];
-}
-?>
 
-include "login.php";
+
+<?php
+include "login.php"
+?>
 
 <form method="post" action="login.php">
  </form>
@@ -38,31 +34,20 @@ Password <input type="password" name="pwd"><br>
 <input type="submit" value="Go">
 
 
-include "registeration.php";
-<form method="post" action="registeration.php"></form>
-<script type='text/javascript'>var user=userName</script>
-<script type='text/javascript'>
-var register = account.Registration;
-<div class='title'><h2>Sign up for a user account today!</h2></div><br><br>
 <?php
-if(isset($_GET['reg'])) {
-$check = false;
-}else{
-$check = true;
-}
-if($check){
-echo "<br><h4>Account created successfully. <a href='login.html'>Click here to log in</a>.</h4><br>";
-}
+include "registeration.php"
 ?>
 
-include "cart.php";
-<form="post" action="cart.php>"
-</form>
+
+<?php
+include "cart.php"
+?>
+
 <h1 class="items">Your CPS Shopping Cart</h1>
 <div class="fl">
 <h2 class="items ">Your CPS Shopping Cart</h2>
 </div>
 
-include "footer.php";
 <?php
+include "footer.php";
 ?>
